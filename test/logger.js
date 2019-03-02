a6cf45bb8a24814ae908d3c7de6a2a1686f1c515
shenyiling <jzx19951028yyjy@163.com>
2019-03-02 17:12:27 +0800

    fix: 测试文件

--- test/logger.js
+++ test/logger.js
@@ -1,13 +1,11 @@
 const request = require('supertest')
 const assert = require('assert')
-const sinon = require('sinon')
-const fs = require('fs')
 const split = require('split')
+const Koa = require('koa')
+const router = require('koa-router')()
+const Logger = require('../dist/index').default
 
-let app, log
-const stream = fs.createWriteStream('app.log', {
-    flags: 'a'
-})
+let app
 
 describe('koa2-request-log', () => {
     describe('log with options', () => {
@@ -17,26 +15,61 @@ describe('koa2-request-log', () => {
                     done(err)
                     return 
                 }
-                // TODO:检验错误
-                // assert(/\\u001b\[38;2;255;255;0m  \\u001b\[39m/.test(line))
+                assert(/\u001b\[38;2;255;255;0m.*\u001b\[39m/.test(line))
                 done()
             })
-            const stream = createLineStream((line) => {
-                // 校验
+            const stream = createTestStream((line) => {
                 cb(null, line)
             })
-            app = require('../test-server')({
+            app = createTestServer({
                 stream,
                 logColor: 'ff0'
             })
             request(app.listen())
                 .get('/normal')
                 .expect(200, cb)
+        }),
+        it('log with custom format', function (done) {
+            const cb = after(2, function (err, line) {
+                if (err) {
+                    done(err)
+                    return 
+                }
+                assert(line === 'GET 200')
+                done()
+            })
+            const stream = createTestStream(line => {
+                cb(null, line)
+            })
+            app = createTestServer({
+                stream,
+                logFmt: ':method :status'
+            })
+            request(app.listen())
+                .get('/normal')
+                .expect(200, cb)
+        }),
+        it('log with skip option', function (done) {
+            const stream = createTestStream(line => {
+                assert(false, 'should not log')
+            })
+            app = require('../test-server')({
+                stream,
+                skip(_, res) {
+                    if (res.status >= 400) {
+                        return true
+                    }
+                    return false
+                }
+            })
+            request(app.listen())
+                .get('/server-error')
+                .expect(500, done)
         })
     })
 })
 
-function createLineStream (callback) {
+function createTestStream (callback) {
     return split().on('data', callback)
 }
 
@@ -53,4 +86,27 @@ function after (count, callback) {
             callback.apply(null, args)
         }
     }
+}
+
+router
+.get('/normal', (ctx) => {
+    ctx.body = 'request normal'
+})
+.get('/client-error', (ctx) => {
+    ctx.response.status = 400
+    ctx.body = 'client error'
+})
+.get('/server-error', (ctx) => {
+    ctx.response.status = 500
+    ctx.body = 'server error'
+})
+
+// return koa server for test
+function createTestServer (opts) {
+    const app = new Koa()
+    const logger = (new Logger()).generate(opts)
+    app.use(logger)
+    app.use(router.routes())
+
+    return app
 }
